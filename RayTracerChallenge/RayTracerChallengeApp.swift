5b5452de0ca303eb384e75e663b57c00cefb5850
Anna Melnikov <anna.me@hey.com>
2023-12-12 18:19:41 -0600

    Initial Commit

---
+++ RayTracerChallenge/RayTracerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayTracerChallengeApp.swift
+//  RayTracerChallenge
+//
+//  Created by Anna Melnikov on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RayTracerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
